78488ac2b7cfe28d7e4a1b3f765de41af425ba55
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2019-03-30 08:44:23 +0000

    Add support for encoding and decoding latitude/longitude moved to Chronicle-Coder

--- src/test/java/net/openhft/chronicle/coder/WordsEncoderTest.java
+++ src/test/java/net/openhft/chronicle/coder/WordsEncoderTest.java
@@ -12,15 +12,20 @@ public class WordsEncoderTest {
     public void london() throws IOException {
         Coder coder = WordsCoderBuilder.fromFile("common-words.txt", 1936).build();
         double precision = 1e-4;
-        System.out.println(coder.asStringForLatLon(51.5074, -0.1278, precision));
-        System.out.println(coder.asStringForLatLon(48.8566, 2.3522, precision));
-        System.out.println(coder.asStringForLatLon(40.7128, -74.0060, precision));
-        System.out.println(coder.asStringForLatLon(-37.8136, 144.9631, precision));
+        System.out.println("london: " + coder.asStringForLatLon(51.5074, -0.1278, precision));
+        System.out.println("paris: " + coder.asStringForLatLon(48.8566, 2.3522, precision));
+        System.out.println("new york: " + coder.asStringForLatLon(40.7128, -74.0060, precision));
+        System.out.println("melbourne: " + coder.asStringForLatLon(-37.8136, 144.9631, precision));
+
+        System.out.println(coder.parseLatLon("may.clear.broadway.fort"));
+        System.out.println(coder.parseLatLon("may.clear.broadway"));
+        System.out.println(coder.parseLatLon("may.clear"));
+        System.out.println(coder.parseLatLon("may"));
     }
 
     @Test
     public void parseLatLon() throws IOException {
-        for (int size : new int[]{16, 36, /*short*/1024, /*medium*/1936, 4096}) {
+        for (int size : new int[]{16, 36, /*short*/1217, /*medium*/2094, 4096}) {
             System.out.println("size: " + size);
             Coder coder = WordsCoderBuilder.fromFile("common-words.txt", size).build();
             for (int x = -90; x <= 90; x += 15) {
@@ -40,7 +45,7 @@ public class WordsEncoderTest {
 
     @Test
     public void parseLongDecimal() throws IOException {
-        Coder coder = WordsCoderBuilder.fromFile("common-words.txt", 16).build();
+        Coder coder = WordsCoderBuilder.fromFile("common-words.txt", 2094).build();
         StringBuilder sb = new StringBuilder();
         for (long l : new long[]{
                 Long.MIN_VALUE, 0xFEDCBA9876543210L, -Long.MAX_VALUE, Integer.MIN_VALUE, -1,
@@ -48,6 +53,7 @@ public class WordsEncoderTest {
             sb.setLength(0);
             coder.appendLong(sb, l);
             assertEquals(l, coder.parseLong(sb));
+            System.out.println(Long.toHexString(l) + ": " + sb);
         }
     }
 }
